73faf56474597827585e825d71dc8de140cf01a5
Timothy Carter <talic2002@gmail.com>
2022-08-31 10:12:21 -0400

    First submission

---
+++ src/Mole.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import moleImg from './images/mole.png'
+
+function Mole (props) {
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.setDisplayMole(false)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div>
+            <img style={{ 'width': '30vw'}}
+            src={moleImg}
+            onClick={props.handleClick} />
+        </div>
+    )
+
+
+}
+
+
+export default Mole
